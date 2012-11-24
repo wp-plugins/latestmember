e9888b7bd82a4bd06abc9abb2f4e3fc82d7c1b1d
progpars <progpars@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-24 12:01:11 +0000

    new version
    git-svn-id: https://plugins.svn.wordpress.org/latestmember/tags/1.0@629383 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ latestmember.php
@@ -0,0 +1,101 @@
+<?php
+/*
+Plugin Name: آخرین کاربران عضو شده 
+Plugin URI: http://wordpress.org/extend/plugins/latestmember/
+Description: نمایش آخرین کاربران عضو شده با استفاده از تکنولوژی جی کوئری 
+Version: 1.0
+Author: مهدی خاکسار
+Author URI: http://www.progpars.com
+License: progpars.com
+*/
+function latestmember_active()
+{
+	add_option("latestmember", 10);	
+}
+register_activation_hook(__FILE__,'latestmember_active');
+
+function latestmember_deactive()
+{
+	delete_option('latestmember');	
+}
+register_deactivation_hook(__FILE__,'latestmember_deactive');
+
+function latestmember()
+{ 	
+	$total = get_option('latestmember');
+	$latestmember_users = get_users('orderby=registered&order=DESC&number='.$total.'');
+        foreach($latestmember_users as $user) {
+		$get_date_registered =  $user->user_registered;
+		$jdate_registdate = jdate(" d / m / Y",strtotime($get_date_registered));
+		$email = $user->user_email;
+		$get_usermail = get_user_by('email',$email);
+		$user_largimg = get_avatar($user->user_email, 150 );	
+		$user_uselargimg = "<img style='float: left; margin-right: 20px;'$user_largimg ";
+		echo 
+		'
+		<div class="members"> 
+		<a class="tip_trigger"> 
+		 '.get_avatar($user->user_email , 50 ).' 
+		<span style="display: none; top: -7px; left: 719px;" class="tip"> 
+		'.$user_uselargimg.'
+		 <strong>شناسه : </strong>'.$user->user_login.'<br />
+		  
+		  <strong>نام کامل : </strong>'.$user->display_name.'<br />
+		  
+		  <strong>تاریخ عضویت : </strong>'.$jdate_registdate.'<br />
+		  
+		  <strong>درباره من : </strong>'.$get_usermail->user_description.'<br />
+		</span></a> 
+		</div>
+		';	
+    }					
+}
+add_shortcode('latestmember','latestmember');
+
+function admin_options_latestmember()
+{
+	$value_totaluser = $_POST['usertotal'];
+	if(isset($_POST['submit']))
+	{
+		update_option('latestmember',$value_totaluser);
+		
+		?>
+        <div id="setting-error-settings_updated" class="updated settings-error"><p>تغییر ذخیره شد</strong></p></div>
+        <?php
+	}
+	?>
+<div class="wrap">
+  <?php screen_icon();?>
+  <h2>تنظیم تعداد نمایش</h2>
+  <p>نمایش آخرین کاربران عضو شده در سایت</p>
+  <form action="" method="post">
+  <label>تعداد نمایش </label>
+      <input type="text" value="<?php echo get_option('latestmember',10);?>" class="small-text" name="usertotal" />
+      <label>مورد</label>
+      <p class="submit"><input type="submit" value="ذخیره" class="button-primary" name="submit"/></p>
+          
+  </form>
+</div>
+<?php
+}
+add_action('admin_menu','admin_menu_latestmember');
+
+function latestmemberJs()
+{
+	wp_enqueue_script('jquery');
+	wp_register_script('latestmemberJs',plugins_url('/latestmember-js.js', __FILE__));
+	wp_enqueue_script('latestmemberJs');
+}
+add_action('wp_enqueue_scripts','latestmemberJs');
+
+function latestmemberStyle()
+{
+	echo '<link rel="stylesheet" href="'.plugins_url('latestmember-style.css',__FILE__).'" type="text/css" media="all" />';
+}
+add_action('wp_head','latestmemberStyle');
+
+function admin_menu_latestmember()
+{
+	add_options_page('تنظیمات آخرین کاربران عضو شده','آخرین کاربران عضو شده','manage_options','latestmember','admin_options_latestmember');
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,29 @@
+=== Plugin Name ===
+Contributors: progpars
+Donate link: http://progpars.com/
+Tags: member, last ,user
+Requires at least: 3.0.1
+Tested up to: 3.4
+Stable tag: 4.3
+License: progpars
+License URI: http://progpars.com
+
+View the latest registered users on the site using jquery
+
+== Description ==
+
+View the latest registered users on the site using jquery
+
+Support: forum.progpars.com and mahdi@progpars.com
+
+== Installation ==
+
+1. Upload Folder to wp-content/plugins/
+2. Activate the plugin
+3. Place `<?php latestmember(); ?>` in your templates and [latestmember] in page or post
+
+== Screenshots ==
+
+<img src="http://www.progpars.com/wp-content/uploads/2012/11/screenshot1.jpg" alt="demo" />
+<img src="http://www.progpars.com/wp-content/uploads/2012/11/screenshot2.jpg" alt="panel" />
+
